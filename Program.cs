aa6f81be0ab2a834c6b90f38fa2a436d1346ee17
antonharkevich <61267091+antonharkevich@users.noreply.github.com>
2020-03-21 17:58:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleAppLaba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] a = s.Split();
+            StringBuilder builder = new StringBuilder();
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                builder.Append(a[i]);
+                builder.Append(" ");
+            }
+            Console.WriteLine(builder);
+        }
+    }
+}
